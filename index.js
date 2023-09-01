f394ce234acd03c18c619721cbde3b4b297275a7
Ali Mahmoudi Dev <alimahmoudidev@gmail.com>
2023-09-01 17:01:42 -0400

    Create mvp app

---
+++ index.js
@@ -0,0 +1,121 @@
+import fs from "fs";
+
+const address = process.argv[2];
+const addressFileIndex = `${address}/index.js`;
+
+// Delete Old File Created
+function deletePreviousFile(addressFileIndex) {
+  fs.unlink(addressFileIndex, (err) => {
+    if (err) {
+      if (err.code == "ENOENT") {
+        console.log("The file did not exist");
+      } else {
+        console.log(err);
+      }
+      return;
+    }
+    console.log("Delete the previous file");
+  });
+}
+
+// Read files in path
+async function readAllFile(path) {
+  const list = [];
+  const files = await new Promise((resolve, reject) => {
+    fs.opendir(path, (err, dir) => {
+      if (err) reject(err);
+      const readNext = () => {
+        dir.read((err, file) => {
+          if (err) reject(err);
+          if (file === null) {
+            resolve(list);
+            return;
+          }
+          const filePath = path + "/" + file.name;
+          if (file.isDirectory()) {
+            readAllFile(filePath).then((files) => {
+              list.push(...files);
+              readNext();
+            });
+          } else {
+            list.push(filePath);
+            readNext();
+          }
+        });
+      };
+      readNext();
+    });
+  });
+  return files;
+}
+
+// Create Name File Camel Case
+function beautifulName(fileName) {
+  let newName = fileName.replaceAll("-", " ").replaceAll("_", " ");
+  const beaUName = newName.split(" ");
+  beaUName.forEach((partName, i) => {
+    if (i == 0) return;
+    if (partName.length == 1) beaUName[i] = partName.toUpperCase();
+    beaUName[i] =
+      partName[0].toUpperCase() + partName.slice(1, partName.length);
+  });
+  return beaUName.join("");
+}
+
+// Find Name File in path File
+function getNameFile(srcFile) {
+  let fileName = srcFile.split("/").pop().split(".");
+  fileName.pop();
+  fileName = fileName.join("");
+  return beautifulName(fileName);
+}
+
+// Create File index
+function createFileText(objFile) {
+  const arryImport = [];
+  for (const key in objFile) {
+    if (Object.hasOwnProperty.call(objFile, key)) {
+      arryImport.push(`import ${key} from '${objFile[key]}'`);
+    }
+  }
+  const objExport = Object.keys(objFile)
+  let obj = "{"
+  objExport.forEach(key=>{
+    obj+=`${key},`
+  })
+  obj+="}"
+  console.log(obj);
+  arryImport.push(`const img = ${obj}`);
+  arryImport.push("export default img");
+  return arryImport;
+}
+
+// Delete
+deletePreviousFile(addressFileIndex);
+// Run Code
+readAllFile(address)
+  // Create Object Files
+  .then((list) => {
+    const objSrcFile = {};
+    list.map((srcFile) => {
+      let fileName = getNameFile(srcFile);
+      if (objSrcFile[fileName] !== undefined) {
+        const folder = srcFile.split("/");
+        fileName = `${folder[folder.length - 2]}_${fileName}`;
+      }
+      objSrcFile[fileName] = srcFile;
+    });
+    return objSrcFile;
+  })
+  // Add To File Index js
+  .then((objSrcFile) => {
+    const importObj = createFileText(objSrcFile);
+    importObj.forEach((line) => {
+      fs.appendFile(addressFileIndex, `\n${line}`, (err) => {
+        if (err) {
+          console.log(err);
+          return;
+        }
+      });
+    });
+  });
---
+++ src/assets/index.js
@@ -0,0 +1,43 @@
+import logo from "./src/assets/imgs/logo.png";
+import pdfBTik from "./src/assets/imgs/pdfB-tik.png";
+import pdfBUsers1 from "./src/assets/imgs/pdfB-users1.png";
+import pdfBWorld from "./src/assets/imgs/pdfB-world.png";
+import pdfBWhatsapp from "./src/assets/imgs/dirc2/pdfB-whatsapp.png";
+import pdfBLink1 from "./src/assets/imgs/dirc2/pdfB-link1.png";
+import pdfBLink from "./src/assets/imgs/dirc2/pdfB-link.png";
+import imgTour from "./src/assets/imgs/imgTour.jpg";
+import imgs_pdfBWhatsapp from "./src/assets/imgs/pdfB-whatsapp.png";
+import logoMini from "./src/assets/imgs/logoMini.png";
+import pdfBClose from "./src/assets/imgs/pdfB-close.png";
+import pdfBUser1 from "./src/assets/imgs/pdfB-user1.png";
+import pdfBPrice from "./src/assets/imgs/pdfB-price.png";
+import pdfBLocation from "./src/assets/imgs/pdfB-location.png";
+import imgs_pdfBLink1 from "./src/assets/imgs/pdfB-link1.png";
+import pdfBAirplane from "./src/assets/imgs/pdfB-airplane.png";
+import pdfBUser2 from "./src/assets/imgs/pdfB-user2.png";
+import pdfBUsers2 from "./src/assets/imgs/pdfB-users2.png";
+import pdfBClock from "./src/assets/imgs/pdfB-clock.png";
+import imgs_pdfBLink from "./src/assets/imgs/pdfB-link.png";
+const img = {
+  logo,
+  pdfBTik,
+  pdfBUsers1,
+  pdfBWorld,
+  pdfBWhatsapp,
+  pdfBLink1,
+  pdfBLink,
+  imgTour,
+  imgs_pdfBWhatsapp,
+  logoMini,
+  pdfBClose,
+  pdfBUser1,
+  pdfBPrice,
+  pdfBLocation,
+  imgs_pdfBLink1,
+  pdfBAirplane,
+  pdfBUser2,
+  pdfBUsers2,
+  pdfBClock,
+  imgs_pdfBLink,
+};
+export default img;
